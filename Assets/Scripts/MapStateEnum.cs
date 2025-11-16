85bd5555beb2a6638f226b2e3bdd0109f9e1b53a
jmiisu <jamiesyh0426@gmail.com>
2025-11-16 18:56:48 +0900

    feat: Grid State Manager (251116)
    -also import TextMesh Pro

--- Assets/Scripts/MapStateEnum.cs
+++ Assets/Scripts/MapStateEnum.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 public enum MAP_STATE
 {
-    EMPTY,
-    STAGE_BLOCK,
-    PLAYER_BLOCK
+    EMPTY,          // 빈 곳
+    STAGE_BLOCK,    // 스테이지 고정 블록
+    BASIC,          // 기본 블록
+    STAIR,          // 계단 블록
+    EXIT,           // 출구
+    PLAYER_POS,     // 플레이어 위치
 };
+
+/*
+ * 0 0 0 0 0 0 
+ * 0 0 0 0 0 0 
+ * 0 0 0 0 0 0
+ * 1 0 2 P 3 2
+ * 1 1 1 1 1 1
+ */
